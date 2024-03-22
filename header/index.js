e0ebdc627ad8d10fd33c320cb8383c7c7b6fab01
Sorawit1234 <sorawit@planforfit.com>
2024-03-22 20:06:41 +0700

    update

--- header/index.js
+++ header/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import logo from "../image/logo.png";
 import user_line from "../image/user-3-line.png";
 import ellipse24 from "../image/Ellipse24.png";
@@ -12,25 +12,40 @@ import Cookies from "js-cookie";
 import "./header.css";
 
 export default function PynkHeader(props) {
+  const isLocalHost = window.location.hostname != "localhost";
   const [groupImage, setGroupImage] = useState(false);
-  const dataCookiesLoginUser = Cookies.get("loginUser");
-  const urlCookieLoginWeb = true ? "pynk.co" : "localhost";
-  let urlProd = true;
-  const urlPynk = urlProd
+  const urlCookieLoginWeb = isLocalHost ? "pynk.co" : "localhost";
+  const dataCookiesLoginUser = Cookies.get("loginUser", {
+    domain: urlCookieLoginWeb,
+    path: "/",
+  });
+  const urlBetterHome = isLocalHost
+    ? "https://staging.pynk.co/#/sale-page?link=bettershape"
+    : "http://localhost:3000/#/sale-page?link=bettershape";
+
+  const urlCarrotHome = isLocalHost
+    ? "https://staging.carrotworkout.pynk.co/#/sale-page?link=getfit"
+    : "http://localhost:3000/#/sale-page?link=getfit";
+
+  const urlPynkProd = isLocalHost
     ? "https://staging.pynk.co/#"
     : "http://localhost:3000/#";
 
-  const urlPreem = urlProd
+  const urlPreemProd = isLocalHost
     ? "https://staging.preemworkout.pynk.co/#"
     : "http://localhost:3001/#";
 
+  const urlCarrotProd = isLocalHost
+    ? "https://staging.carrotworkout.pynk.co/#"
+    : "http://localhost:3001/#";
+
   const handleLogin = () => {
-    window.location.href = urlPynk + "/login";
+    window.location.href = urlPynkProd + "/login";
   };
 
   const handleOnUserLogout = () => {
     console.log("handle OnUser Logout");
-    window.location.href = urlPynk + "/home";
+    window.location.href = urlPynkProd + "/home";
     Cookies.remove("loginUser", { domain: urlCookieLoginWeb, path: "/" });
 
     if (document.getElementById("icon-google")) {
@@ -38,50 +53,38 @@ export default function PynkHeader(props) {
     }
   };
 
-  const handleClickLogin = async (event) => {
-    const checkMail = Cookies.get("loginUser", {
-      domain: urlCookieLoginWeb,
-      path: "/",
-    });
-    const urlBetter = "https://staging.pynk.co/#/sale-page?link=bettershape";
-    // const urlBetter = "http://localhost:3000/#/sale-page?link=bettershape";
-
+  const handleClickLoginPreem = async () => {
     const response = await fetch(
-      `https://api.planforfit.com/preem/login?email=${checkMail}`
+      `https://api.planforfit.com/preem/login?email=${dataCookiesLoginUser}`
     );
+
     const data = await response.json();
-    console.log("data", data);
+
     if (data.results.message == "success") {
       const encodedParams = btoa(JSON.stringify(dataCookiesLoginUser));
       window.location.href =
-        urlPreem + `/videolist?encodedParams=${encodedParams}`;
+        urlPreemProd + `/videolist?encodedParams=${encodedParams}`;
     } else {
-      window.location.href = urlBetter;
+      window.location.href = urlBetterHome;
     }
-    // fetch(`https://api.planforfit.com/preem/login?email=${checkMail}`)
-    //   .then((res) => {
-    //     console.log('res', res)
-    //   })
-    //   .then((response) => {
-    //     const res = response.data.results;
-    //     console.log("data", response);
+  };
 
-    //     if (res.message == "success") {
-    //       console.log("data", response);
-    //       const encodedParams = btoa(JSON.stringify(dataCookiesLoginUser));
-    //       window.location.href =
-    //         urlPreem + `/videolist?encodedParams=${encodedParams}`;
-    //     } else {
-    //       window.location.href = urlBetter;
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     return err;
-    //   });
+  const handleClickLoginCarrot = async () => {
+    const response = await fetch(
+      `https://api.planforfit.com/fit_carrot/login?email=${dataCookiesLoginUser}`
+    );
+    const data = await response.json();
+    if (data.results.message == "success") {
+      const encodedParams = btoa(JSON.stringify(dataCookiesLoginUser));
+      window.location.href =
+        urlCarrotProd + `/videolist?encodedParams=${encodedParams}`;
+    } else {
+      window.location.href = urlCarrotHome;
+    }
   };
 
   const handleBackHome = () => {
-    window.location.href = urlPynk + "/Home";
+    window.location.href = urlPynkProd + "/Home";
   };
 
   return (
@@ -298,8 +301,15 @@ export default function PynkHeader(props) {
                 Stay Fit
               </a>
 
-              <a className="nav-link link-pynk" onClick={handleClickLogin}>
+              <a className="nav-link link-pynk" onClick={handleClickLoginPreem}>
                 Better Shape
+              </a>
+
+              <a
+                className="nav-link link-pynk"
+                onClick={handleClickLoginCarrot}
+              >
+                Carrot
               </a>
 
               <a
